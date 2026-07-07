41726e04396223704db6f6517bb0b273cecbd49c
Sevada <64130935+Sevada797@users.noreply.github.com>
2026-07-07 19:44:51 +0400

    Create config.php for OAuth2 client credentials
    Initial configuration file for OAuth2 client setup.

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright (C) 2013 Tony Gaitatzis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Tony Gaitatzis - http://www.linkedin.com/in/tonygaitatzis
+ * This code complements the online tutorial:
+ * http://20missionglass.tumblr.com/post/60787835108/programming-an-oauth2-client-app-in-php
+ */
+
+// Acc 1 setup, you can try with several creds to understand some attack cases
+$oauth2_client_id = '<YOUR_OAUTH_CLIENT_ID>'; // Client ID  replace with yours 
+$oauth2_secret = '<YOUR_OAUTH_SECRET>';  // Client Secret  replace with yours
+$oauth2_redirect = '<YOUR_OAUTH_REDIRECT>'; // Redirect URI / Callback  replace with yours
+
+
+?>
